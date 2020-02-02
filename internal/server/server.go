517262cf67152b5e60b8d43c7f3c68fd169f62e8
dagowa <sazhinanatalyatobi@yandex.ru>
2020-02-03 00:58:33 +0700

    ListAddvert, GetAdvert were added

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,39 @@ package server
 
 import (
 	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/rs/zerolog/hlog"
 
 	"github.com/dagowa/adservice/internal/chi_utils"
+	"github.com/dagowa/adservice/internal/controllers/advertmanager"
+	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 )
 
 type server struct {
+	service *service
 }
 
 func (s *server) ListAdverts(w http.ResponseWriter, r *http.Request) {
-	render.Respond(w, r, chi_utils.NotImplementedError())
+	logger := hlog.FromRequest(r)
+
+	am := s.service.AdvManager
+
+	sortCriteria := advertmanager.SortCriteria{}
+	sortCriteria.SetDateSortOrder(advertmanager.SortOrderASC)
+
+	ctx := r.Context()
+	pnumb := ctx.Value("pnumb")
+
+	adverts, err := am.GetBatch(&sortCriteria, pnumb.(int), 10)
+	if err != nil {
+		logger.Error().Err(err).Msg("Cannot get batch of adverts")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	render.JSON(w, r, adverts)
 }
 
 func (s *server) AddAdvert(w http.ResponseWriter, r *http.Request) {
@@ -19,5 +42,34 @@ func (s *server) AddAdvert(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) GetAdvert(w http.ResponseWriter, r *http.Request) {
-	render.Respond(w, r, chi_utils.NotImplementedError())
+	logger := hlog.FromRequest(r)
+
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+
+	q := r.URL
+	params, err := url.ParseQuery(q.RawQuery)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	rf := advertmanager.RequirementFileds{}
+	if params.Get("date") != "" {
+		rf.Date = true
+	}
+	if params.Get("descr") != "" {
+		rf.Description = true
+	}
+	if params.Get("gallery") != "" {
+		rf.Gallery = true
+	}
+
+	am := s.service.AdvManager
+	adverts, err := am.GetOne(id, &rf)
+	if err != nil {
+		logger.Error().Err(err).Msg("Cannot get advert")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	render.JSON(w, r, adverts)
 }
--- internal/server/service.go
+++ internal/server/service.go
@@ -5,24 +5,47 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/dagowa/adservice/middleware/timings"
+	"github.com/dagowa/adservice/internal/controllers/advertmanager"
+	"github.com/dagowa/adservice/internal/storage"
+	"github.com/dagowa/adservice/middleware"
+	"github.com/rs/zerolog"
+
 	"github.com/dagowa/adservice/pkg/logger"
 
 	httplogger "github.com/dagowa/adservice/pkg/logger/http"
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	chimiddleware "github.com/go-chi/chi/middleware"
 )
+
+type service struct {
+	AdvManager *advertmanager.AdvertManager
+}
 
 // NewServer is ...
 func NewServer(ctx context.Context, cfg *Config) *http.Server {
 	r := chi.NewRouter()
 	httpSrv := &http.Server{Addr: cfg.Host + ":" + strconv.Itoa(cfg.Port), Handler: r}
 
-	srv := &server{}
+	l := zerolog.Ctx(ctx)
+
+	psqlConn, err := storage.New().NewPostgreSQLConn(cfg.PSQLConfig)
+	if err != nil {
+		l.Fatal().Err(err).Msg("Cannot set up psql connection")
+	}
+	//TODO: ой блять, наверное надо отсюда его вытащить -_-
+	defer psqlConn.Close()
+
+	service := &service{
+		AdvManager: &advertmanager.AdvertManager{ConnPool: psqlConn.Pool()},
+	}
+	srv := &server{
+		service: service,
+	}
+	md := middleware.Middleware{ConnPool: psqlConn.Pool()}
 
 	{
 		r.Use(httplogger.NewHandler(*logger.Ctx(ctx)))
-		r.Use(timings.ElapsedTime)
+		r.Use(md.ElapsedTime)
 		r.Use(httplogger.RequestIDHandler("id_request", "X-Request-ID"))
 		r.Use(httplogger.Recoverer)
 		if cfg.LogRequests {
@@ -30,10 +53,10 @@ func NewServer(ctx context.Context, cfg *Config) *http.Server {
 		}
 		r.Get("/adverts", srv.ListAdverts)
 		r.Post("/adverts", srv.AddAdvert)
-		r.Get("/advert/{id}", srv.GetAdvert)
+		r.With(md.AdvertID).Get("/advert/{id}", srv.GetAdvert)
 
 		r.Route("/internal", func(r chi.Router) {
-			r.Mount("/debug", middleware.Profiler())
+			r.Mount("/debug", chimiddleware.Profiler())
 		})
 	}
 
